b4e6ceef653acf1b334d52b51673deaf87b793c5
aorohith <aorohith@gmail.com>
2022-05-25 12:43:26 +0530

    Added video in fastlaugh section

--- lib/presentation/fast_laugh/widgets/video_list_item.dart
+++ lib/presentation/fast_laugh/widgets/video_list_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:netflix/core/colors/colors.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoListItem extends StatelessWidget {
   final int index;
@@ -8,12 +9,24 @@ class VideoListItem extends StatelessWidget {
     required this.index,
   }) : super(key: key);
 
+  var videoUrlList = [
+    'https://assets.mixkit.co/videos/preview/mixkit-girl-in-neon-sign-1232-large.mp4',
+    'https://assets.mixkit.co/videos/preview/mixkit-portrait-of-a-woman-in-a-pool-1259-large.mp4',
+    'https://assets.mixkit.co/videos/preview/mixkit-mother-with-her-little-daughter-eating-a-marshmallow-in-nature-39764-large.mp4',
+    'https://assets.mixkit.co/videos/preview/mixkit-red-frog-on-a-log-1487-large.mp4',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         Container(
-          color: Colors.accents[index % Colors.accents.length],
+          // color: Colors.accents[index % Colors.accents.length],
+          child: FastVideoPlayer(
+            index: index,
+            videoUrls: videoUrlList,
+            onStateChanged: (bool) {},
+          ),
         ),
         Align(
           alignment: Alignment.bottomCenter,
@@ -91,5 +104,54 @@ class VideoActionsWidget extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class FastVideoPlayer extends StatefulWidget {
+  final List<String> videoUrls;
+  final index;
+  final void Function(bool isPlaying) onStateChanged;
+  const FastVideoPlayer({
+    Key? key,
+    required this.index,
+    required this.videoUrls,
+    required this.onStateChanged,
+  }) : super(key: key);
+
+  @override
+  State<FastVideoPlayer> createState() => _FastVideoPlayerState();
+}
+
+class _FastVideoPlayerState extends State<FastVideoPlayer> {
+  late VideoPlayerController _videoController;
+
+  @override
+  void initState() {
+    _videoController = VideoPlayerController.network(
+        widget.videoUrls[widget.index % widget.videoUrls.length]);
+    _videoController.initialize().then((value) {
+      setState(() {
+        _videoController.play();
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: _videoController.value.isInitialized
+            ? AspectRatio(
+              aspectRatio: _videoController.value.aspectRatio,
+                child: VideoPlayer(_videoController),
+              )
+            : Center(child: CircularProgressIndicator()));
+  }
+  @override
+  void dispose() {
+    _videoController.dispose();
+    super.dispose();
   }
 }
